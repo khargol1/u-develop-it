1e6f8ac2bad93853254bc183002e79bf40bed112
Khargol <josephklein3@gmail.com>
2020-08-17 11:53:10 -0400

    join queries

--- server.js
+++ server.js
@@ -44,7 +44,11 @@ app.get('/api/candidate/:id', (req, res) => {
 
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
     const params = [];
     db.all(sql, params, (err, rows) => {
         if (err) {
@@ -61,7 +65,12 @@ app.get('/api/candidates', (req, res) => {
 
 // Delete a candidate
 app.delete('/api/candidate/:id', (req, res) => {
-    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.run(sql, params, function (err, result) {
         if (err) {
